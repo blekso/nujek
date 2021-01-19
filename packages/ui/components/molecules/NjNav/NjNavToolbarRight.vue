85e3277e0700d03c23749ebb0b16494403df2204
blekso <mihael.istvan@gmail.com>
2021-01-19 09:59:07 +0100

    added toolbarright navbar

---
+++ packages/ui/components/molecules/NjNav/NjNavToolbarRight.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class="getThemeClass('wrapper')">
+    <slot name="header">
+      <header :class="getThemeClass('header')">
+        <div class="col-span-4 flex">
+          <slot name="burger-menu" />
+          <slot name="logo" />
+          <slot name="nav" />
+        </div>
+        <div class="flex justify-end">
+          <slot name="toolbar" />
+        </div>
+      </header>
+    </slot>
+  </div>
+</template>
+<script>
+import Component from '../../../base/Component'
+import NjBurger from '../../atoms/NjBurger'
+import Vue from 'vue'
+import NjNavItem from './NjNavItem'
+Vue.component('NjNavItem', NjNavItem)
+
+const NjNavToolbarRight = Component.extend({
+  name: 'NjNavToolbarRight',
+  components: {
+    NjBurger
+  },
+  props: {
+    burgerOpen: {
+      type: Boolean,
+      default: false
+    },
+    classes: {
+      type: Object,
+      default() {
+        return {
+          wrapper: 'w-full',
+          header: 'grid md:grid-cols-6'
+        }
+      }
+    }
+  }
+})
+export default NjNavToolbarRight
+</script>
